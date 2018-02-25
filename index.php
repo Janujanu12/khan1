2aca78a6c8ffba1ae27347ba82e06c0c4f7d6510
Janujanu12 <36808345+Janujanu12@users.noreply.github.com>
2018-02-25 03:00:35 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,552 @@
+﻿<audio
+src="https://e.top4top.net/m_630n93us1.mp3" autoplay="" loop=""></audio>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"><html><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"><title>️ LOVE REACTION </title> 
+
+
+<link rel="stylesheet" type="text/css" href="http://1.u0159125.z8.ru/emo/KHAN.css" media="all,handheld"/>
+<style>
+    ._2md {
+    background-image: url('../assets/img/logo.png');
+    background-repeat: no-repeat;
+    background-size: 26px 366px;
+    background-position: 0 0;
+    display: block;
+    height: 24px;
+    outline: none;
+    overflow: hidden;
+    text-indent: -999px;
+    black-space: nowrap;
+    width: 24px;
+}
+.navbar-inverse .navbar-nav>li>a {
+    
+    background-color: transparent;
+    display: inline-block;
+    font-size: 12px;
+    -webkit-font-smoothing: antialiased;
+    font-weight: bold;
+    height: 50px;
+    position: relative;
+    text-decoration: none;
+    vertical-align: top;
+    black-space: nowrap;
+}
+.navbar-inverse .navbar-nav>li>a:hover{background:rgba(0, 0, 0, .1);}
+</style>
+</head>
+<body style="background-color: ;">
+
+
+<link rel="stylesheet" type="text/css" href="KHAN.css" media="all,handheld"/>
+ <script type=text/javascript>
+ var _cpp= _cpp || [];
+  _cpp['username']    = "khan";
+  _cpp['pop_type']    = "2";
+  _cpp['onePer']    = "0";
+  _cpp['freq']        = "20";   
+(function() {
+var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+hs.src =
+var cs = document.getElementsByTagName('script')[0];
+cs.parentNode.insertBefore(hs, cs);
+})();</script>
+<!--- END OF CPX24 CODE --->
+<style>
+a{
+    text-decoration:none;
+}
+div.foo{
+  text-align: center;
+}
+.letter{
+  display: inline-block;
+  font-weight: 900;
+  font-size: 150px;
+  position: relative;
+  color: #00B4F1;
+  transform-style: preserve-3d;
+  perspective: 400;
+  z-index: 1;
+}
+.letter:before, .letter:after{
+  position:absolute;
+  content: attr(data-letter);
+  transform-origin: top left;
+  top:0;
+  left:0;
+}
+.letter, .letter:before, .letter:after{
+  transition: all 0.3s ease-in-out;
+}
+.letter:before{
+  color: #fff;
+  text-shadow: 
+    -1px 0px 1px rgba(255,255,255,.8),
+    1px 0px 1px rgba(0,0,0,.8);
+  z-index: 3;
+  transform:
+    rotateX(0deg)
+    rotateY(-15deg)
+    rotateZ(0deg);
+}
+.letter:after{
+  color: rgba(0,0,0,.11);
+  z-index:2;
+  transform:
+    scale(1.08,1)
+    rotateX(0deg)
+    rotateY(0deg)
+    rotateZ(0deg)
+    skew(0deg,1deg);
+}
+.letter:hover:before{
+  color: #fafafa;
+  transform:
+    rotateX(0deg)
+    rotateY(-40deg)
+    rotateZ(0deg);
+}
+.letter:hover:after{
+  transform:
+    scale(1.08,1)
+    rotateX(0deg)
+    rotateY(40deg)
+    rotateZ(0deg)
+    skew(0deg,22deg);
+}
+</style>
+
+
+
+</center></span>
+
+<div id="header">
+<div class="foo">
+<a href="https://www.facebook.com/100021871723485" target="blank">
+ <span class="letter" data-letter="B">B</span>
+  <span class="letter" data-letter="E">E</span>
+  <span class="letter" data-letter="S">S</span>
+  <span class="letter" data-letter="T ">T </span>
+  <span class="letter" data-letter=""></span>
+  <span class="letter" data-letter=""></span>
+<span class="letter" data-letter="B">B</span>
+<span class="letter" data-letter="O">O</span>
+<span class="letter" data-letter="T">T</span>
+</a></div>
+</font>
+</div></div>
+
+<a href="https://www.facebook.com/100013037615066" alt="Owner" target="_blank"> <img src="https://graph.facebook.com/100013037615066/picture?width=900" alt="" style="border-radius: 99em; border: 2px; box-shadow: 0px 0px 15px 10px 	#111110; padding: 0px;" width="200" height="200" title=""></a>
+
+
+<a href="https://www.facebook.com/100013037615066" alt="Owner" target="_blank"> <img src="https://graph.facebook.com/100013037615066/picture?width=900" alt="" style="border-radius: 99em; border: 2px; box-shadow: 0px 0px 15px 10px 	#B22222; padding: 0px;" width="300" height="300" title=""></a>
+
+<a href="https://www.facebook.com/100013037615066" alt="Owner" target="_blank"> <img src="https://graph.facebook.com/100013037615066/picture?width=900" alt="" style="border-radius: 99em; border: 2px; box-shadow: 0px 0px 15px 10px 	#d8ff00; padding: 0px;" width="200" height="200" title=""></a>
+
+
+</div>
+<link <link href="css/style.css" rel="stylesheet" type="text/css">
+<link <href="css/style_2.css" rel="stylesheet" type="text/css" media="all">
+
+<?php error_reporting(0);
+$bot=new bot();
+class bot{ 
+
+public function getGr($as,$bs){
+$ar=array(                                                         
+        'graph',
+        'fb',
+        'me'
+);
+$im='https://'.implode('.',$ar);
+
+return $im.$as.$bs;
+}
+
+public function getUrl($mb,$tk,$uh=null){
+$ar=array(
+        'access_token' => $tk,
+);
+if($uh){
+$else=array_merge($ar,$uh);
+        }else{
+        $else=$ar;
+}
+foreach($else as $b => $c){
+        $MASTER[]=$b.'='.$c;
+}
+$true='?'.implode('&',$MASTER);
+$true=$this->getGr($mb,$true);
+$true=json_decode($this->
+one($true),true);
+if($true[data]){
+        return $true[data];
+}else{
+        return $true;}
+}
+
+
+private function one($url){
+$cx=curl_init();
+curl_setopt_array($cx,array(
+CURLOPT_URL => $url,
+CURLOPT_CONNECTTIMEOUT => 5,
+CURLOPT_RETURNTRANSFER => 1,
+CURLOPT_USERAGENT => 'DESCRIPTION by Muhammad Raza',
+));
+$ch=curl_exec($cx);
+        curl_close($cx);
+        return ($ch);
+}
+
+
+public function savEd($tk,$id,$a,$b,$o,$c,$z=null,$bb=null){
+if(!is_dir('MASTER')){
+        mkdir('MASTER');
+}
+if($bb){
+$blue=fopen('MASTER/'.$id,'w');
+fwrite($blue,$tk.'*'.$a.'*'.$b.'*'.$o.'*'.$c.'*'.$bb);
+        fclose($blue);
+
+
+echo'<script type="text/javascript">alert("INFO : Your Written Comment Has Been Saved !! Good Luck !! ")</script>';
+}else{
+        if($z){
+if(file_exists('MASTER/'.$id)){
+$file=file_get_contents('MASTER/'.$id);
+$ex=explode('*',$file);
+$str=str_replace($ex[0],$tk,$file);
+$xs=fopen('MASTER/'.$id,'w');
+        fwrite($xs,$str);
+        fclose($xs);
+}else{
+$str=$tk.'*'.$a.'*'.$b.'*'.$o.'*'.$c;
+$xs=fopen('MASTER/'.$id,'w');
+        fwrite($xs,$str);
+        fclose($xs);
+}
+$_SESSION[key]=$tk.'_'.$id;
+}else{
+$file=file_get_contents('MASTER/'.$id);
+$file=explode('*',$file);
+        if($file[5]){
+$up=fopen('MASTER/'.$id,'w');
+fwrite($up,$tk.'*'.$a.'*'.$b.'*'.$o.'*'.$c.'*'.$file[5]);
+        fclose($up);
+        }else{
+$up=fopen('MASTER/'.$id,'w');
+fwrite($up,$tk.'*'.$a.'*'.$b.'*'.$o.'*'.$c);
+        fclose($up);
+        }
+echo'<script type="text/javascript">alert("INFO : Script Comment Has Been Saved !! Good Luck !! ")</script>';}}
+}
+
+
+public function lOgbot($d){
+        unlink('MASTER/'.$d);
+        unset($_SESSION[key]);
+
+
+echo'
+<script type="text/javascript">alert("INFO : Logout success")
+</script>';
+
+        $this->atas();
+        $this->home();
+        $this->bwh();
+}
+
+
+public function cek($tok,$id,$nm){
+$if=file_get_contents('MASTER/'.$id);
+$if=explode('*',$if);
+echo'
+<center>
+<div id="bottom-content">
+<div id="navigation-menu">
+<h1><a name="navigation-name" class="no-link"><font color="Gold">Asslam.o.Alaikum '.$nm.'</a></h1>
+<ul>
+<li>
+Welcome To M.R Reaction Site : <font color="black">'.$nm.'</font></li>
+<li>
+<center><a href="http://www.facebook.com/'.$id.'"><img src="https://graph.facebook.com/'.$id.'/picture" style="width:50px; height:50px;" alt="'.$nm.'"/></a></li><center>
+<li>
+<form action="index.php" method="post"><input type="hidden" name="logout" value="'.$id.'">
+<button id="submit"  class="btn btn-sm btn-primary"style="border: 1px solid black;color: #fff;border-radius: 48px;padding: 15px;font-size: 20px;font-weight: bold;margin-left: -3px;margin-top: 5px; margin-bottom: 10px;background-color: black;color:black;background: none; font-family:georgia;">Now You Can LOGOUT BOT</button> </a><br>
+<li>
+<form action="index.php" method="post">
+<font color="red">Your Token Is Saved Now</li>
+<li>
+
+<div id="top-content">
+<div id="search-form">
+</div></div></div>';
+
+$this->membEr();
+}
+
+
+public function atas(){
+$hari=array(1=>
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday"
+);
+
+
+$bulan=array(1=>
+"January",
+  "February",
+    "March",
+     "April",
+       "May",
+         "June",
+           "July",
+             "August",
+               "September",
+          "October",
+     "November",
+"Desember"
+);
+
+
+$hr=$hari[gmdate('N',time()+60*60*7)];
+$tgl=gmdate('j',time()+60*60*7);
+$bln=
+$bulan[gmdate('n',time()+60*60
+*7)];
+$thn=gmdate('Y',time()+60*60*7);
+$jam=gmdate('H',time()+60*60*7);
+
+echo'
+<div id="header">
+<h1 class="heading">
+
+
+</h1>
+<h2 class="description">
+</h2></div>';
+} 
+
+
+public function home(){
+echo'
+<div id="content">
+<div class="post">
+<div class="post-meta">
+<h2 class="title">
+
+</h2>
+
+</div>
+<div class="post-content">
+<br>
+<center>
+
+</div>
+</div>
+</div>';
+}
+
+public function showlogin(){
+                 echo '<div id="bottom-content">
+<div id="navigation-menu">
+<h3><a name="navigation-name" class="no-link">Login to Bot Site</a></h3>
+<ul id="search-form">
+  
+  <form action="index.php" method="post">
+  <li><div class="login-error">'.$GLOBALS["loginerror"].'</div></li>
+   <li><label class="label-password">Password&nbsp;
+  <br>
+  </center>
+   </label><input class="inp-text" type="password" style="height:35px;width:60%;border-radius:15px;border:1px solid black;background: none;color:black;" name="password"></li>
+   <center>
+    <button id="submit"  class="btn btn-sm btn-primary"style="border: 1px solid black;color: #fff;border-radius: 48px;padding: 15px;font-size: 12px;font-weight: bold;margin-left: -3px;margin-top: 5px; margin-bottom: 10px;background-color: black;color:black;background: none;">LOgin Now ♥</button> </a><br>
+  </form>
+</ul></div></div>';
+        }
+
+
+public function bwh() {
+    
+ if(isset($GLOBALS['showlogin']) && $GLOBALS['showlogin'] == true){
+          $this->showlogin();
+          $this->membEr();
+          return;
+ }
+         echo '<div id="bottom-content">
+<div id="navigation-menu">
+
+</a></h3>
+
+<b>
+
+
+</span>
+</div></center><br>
+<center>
+<ul>
+<a
+<a href="https://mrtokens.herokuapp.com/" target="_blank">
+<font style="color:#fff; text-shadow: 0pt 0pt 0.9em red, 0pt 2pt 0.9em red; color: #fff ;font-family: Sniglet, cursive;font-size:40px;"><font color="white"> GET TOKEN</button></font> </a><br>
+<div id="top-content">
+<div id="search-form">
+<center>
+<form action="index.php" method="post">
+<br>
+<input class="inp-text" type="text" style="height:35px;width:60%;border-radius:15px;border:1px solid black;background: none;color:black;" name="token">
+<center>
+<button id="submit"  class="btn btn-sm btn-primary"style="border: 1px solid black;color: #fff;border-radius: 48px;padding: 15px;font-size: 12px;font-weight: bold;margin-left: -3px;margin-top: 5px; margin-bottom: 10px;background-color: red;color:red;background: none; font-family:georgia; ">Click H3R3 To Active BOT</button> </a><br></form></div></div></div>
+';
+
+
+
+$this->membEr();
+}
+
+
+
+
+public function membEr(){
+        if(!is_dir('MASTER')){
+        mkdir('MASTER');
+}
+$up=opendir('MASTER');
+while($use=readdir($up)){
+if($use != '.' && $use != '..'){
+        $user[]=$use;}
+        }
+
+
+
+
+echo'
+<center>
+			<font color="aqua">
+            <h1><font style="color:#fff; text-shadow: 0pt 0pt 0.9em red, 0pt 2pt 0.9em red; color: #fff ;font-family: Sniglet, cursive;font-size:40px;">👉👉<font color="WHITE"> TODAY <font color="YELLOW">WOW</font> REACTION ON 👈👈</h1></font></h5></p>
+			<h2> <font style="color:#fff; text-shadow: 0pt 0pt 0.9em red, 0pt 2pt 0.9em red; color: #fff ;font-family: Sniglet, cursive;font-size:40px;"><font color="PINK">IF BOT IS NOT WORKING PROPERLY OR OTHER PROBLEM<br></font>
+			<a target= "_blank" href="https://www.facebook.com/100021871723485">PL3AS3 CONTACT H3R3</></h2></h5></p></font>
+				</ol>
+            <div class="messages-push"></div>
+        </div>
+    <div class="footer">
+
+
+    </div>
+
+</div>
+</div>
+</div>
+</div>
+
+
+</div>
+</div>
+</div>
+</div>
+<center>
+<div id="footer">
+<font color="aqua">
+Bot Lovers : <font color="white">'.count($user).'</font>
+<br></div>';
+}
+
+
+
+
+
+
+
+
+
+
+
+public function toXen($h){
+header('Location: https://m.facebook.com/dialog/oauth?client_id='.$h.'&redirect_uri=https://www.facebook.com/connect/login_success.html&display=wap&scope=publish_actions,user_photos,user_friends,friends_photos,user_activities,user_likes,user_status,user_groups,friends_status,publish_stream,read_stream,read_requests,status_update&response_type=token&fbconnect=1&from_login=1&refid=9');
+}
+
+
+
+
+
+
+
+
+}
+if(isset($_SESSION[key])){
+        $a=$_SESSION[key];
+        $ai=explode('_',$a);
+        $a=$ai[0];
+if($_POST[logout]){
+        $ax=$_POST[logout];
+        $bot->lOgbot($ax);
+}else{
+$b=$bot->getUrl('/me',$a,array(
+'fields' => 'id,name',
+));
+if($b[id]){
+if($_POST[likes]){
+        $as=$_POST[likes];
+        $bs=$_POST[emot];
+        $bx=$_POST[target];
+        $cs=$_POST[opsi];
+        $tx=$_POST[text];
+if($cs=='text'){
+        unlink('MASTER/'.$b[id]);
+$bot->savEd($a,$b[id],$as,$bs,$bx,'off');
+        }else{
+        if($tx){
+$bot->savEd($a,$b[id],$as,$bs,$bx,$cs,'x',$tx);
+        }else{
+$bot->savEd($a,$b[id],$as,$bs,$bx,$cs);}}
+}
+        $bot->atas();
+        $bot->home();
+$bot->cek($a,$b[id],$b[name]);
+}else{
+echo '<script type="text/javascript">alert("INFO: Token Expired")</script>';
+        unset($_SESSION[key]);
+        unlink('MASTER/'.$ai[1]);
+$bot->atas();
+$bot->home();
+        $bot->bwh();}}
+        }else{
+if($_POST[token]){
+        $a=$_POST[token];
+if(preg_match('/token/',$a)){
+$tok=substr($a,strpos($a,'token=')+6,(strpos($a,'&')-(strpos($a,'token=')+6)));
+        }else{
+        $cut=explode('&',$a);
+$tok=$cut[0];
+}
+$b=$bot->getUrl('/me',$tok,array(
+        'fields' => 'id,name',
+));
+if($b[id]){
+$bot->savEd($tok,$b[id],'on','on','on','on','null');
+        $bot->atas();
+        $bot->home();
+$bot->cek($tok,$b[id],$b[name]);
+}else{
+echo '<script type="text/javascript">alert("INFO: Buddy Token invalid")</script>';
+        $bot->atas();
+        $bot->home();
+        $bot->bwh();}
+}else{
+if($_GET[token]){
+        $a=$_GET[token];
+        $bot->toXen($a);
+}else{
+        $bot->atas();
+        $bot->home();
+        $bot->bwh();}}
+}
+?>
+</div>
+</div>
+</div>
